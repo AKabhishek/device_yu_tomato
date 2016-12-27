a4735a6a12970a027e9197ff900079f5bb7e04dd
DarkKnight6499 <yazad.madan@gmail.com>
2016-12-27 11:19:43 +0000

    XOSP!
    Change-Id: I486343a19d5200051f8f7dace818369e364440b5

--- xosp.mk
+++ xosp.mk
@@ -14,13 +14,14 @@
 
 $(call inherit-product, device/yu/tomato/full_tomato.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common XOSP stuff.
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
 
-PRODUCT_NAME := cm_tomato
+PRODUCT_NAME :=  xosp_tomato
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := tomato
 
